75e437e1980a132f0566206386cfbd8bbe4104cf
bob7l <bob7lbob7l@yahoo.com>
2012-11-24 20:28:36 -0800

    Fixed "from" returns for AIR

--- src/uk/co/oliwali/HawkEye/entry/BlockChangeEntry.java
+++ src/uk/co/oliwali/HawkEye/entry/BlockChangeEntry.java
@@ -43,7 +43,7 @@ public class BlockChangeEntry extends DataEntry {
 
 	@Override
 	public String getStringData() {
-		if (from == null) return BlockUtil.getBlockStringName(to);
+		if (((from) == null) || (BlockUtil.getBlockStringName(from) == "AIR")) return BlockUtil.getBlockStringName(to);
 		return BlockUtil.getBlockStringName(from) + " changed to " + BlockUtil.getBlockStringName(to);
 	}
 
