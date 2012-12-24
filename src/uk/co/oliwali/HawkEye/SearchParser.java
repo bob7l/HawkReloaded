f8fa6544d0fc11d3b8ab49ee5ceaafde120e1367
bob7l <bob7lbob7l@yahoo.com>
2012-12-23 19:06:13 -0800

    Fixed datefrom

--- src/uk/co/oliwali/HawkEye/SearchParser.java
+++ src/uk/co/oliwali/HawkEye/SearchParser.java
@@ -198,11 +198,10 @@ public class SearchParser {
 						cal.add(Calendar.MINUTE, -1 * mins);
 						cal.add(Calendar.SECOND, -1 * secs);
 						SimpleDateFormat form = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-						//No need for DateFrom here, they can use the date format for that
-						//This is better for purging data
-						dateTo = form.format(cal.getTime());
-
+						
+						dateFrom = form.format(cal.getTime());
 					}
+					
 					//If the time is in the format 'yyyy-MM-dd HH:mm:ss'
 					else if (type == 1) {
 						if (values.length == 1) {
