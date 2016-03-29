a5ffc70112144f501b7aef4c994725e3d11761df
BuildTools <bob7lbob7l@yahoo.com>
2016-03-29 15:01:56 -0700

    Update API

--- src/uk/co/oliwali/HawkEye/util/HawkEyeAPI.java
+++ src/uk/co/oliwali/HawkEye/util/HawkEyeAPI.java
@@ -3,7 +3,6 @@ package uk.co.oliwali.HawkEye.util;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import uk.co.oliwali.HawkEye.DataType;
 import uk.co.oliwali.HawkEye.SearchParser;
 import uk.co.oliwali.HawkEye.callbacks.BaseCallback;
@@ -15,54 +14,67 @@ import uk.co.oliwali.HawkEye.entry.DataEntry;
 /**
  * API for other plugins.
  * See the wiki for information on how to correctly access this API: https://github.com/oliverw92/HawkEye/wiki/
+ *
  * @author oliverw92
  */
 public class HawkEyeAPI {
 
-	/**
-	 * Add a custom entry to the HawkEye database
-	 * @param plugin instance of your plugin (i.e. just pass 'this' from your main class)
-	 * @param action action that has been performed (e.g. Go Home)
-	 * @param player player that performed the action
-	 * @param loc location of the event
-	 * @param data data relevant to the event
-	 */
-	public static boolean addCustomEntry(JavaPlugin plugin, String action, Player player, Location loc, String data) {
-		return addCustomEntry(plugin, action, player.getName(), loc, data);
-	}
-	public static boolean addCustomEntry(JavaPlugin plugin, String action, String player, Location loc, String data) {
-		if (plugin == null || action == null || player == null || loc == null || data == null) return false;
-		DataEntry entry = new DataEntry(player, DataType.OTHER, loc, action + "-" + data);
-		return addEntry(plugin, entry);
-	}
+    /**
+     * Add a custom entry to the HawkEye database
+     *
+     * @param plugin instance of your plugin (i.e. just pass 'this' from your main class)
+     * @param action action that has been performed (e.g. Go Home)
+     * @param player player that performed the action
+     * @param loc    location of the event
+     * @param data   data relevant to the event
+     */
+    @Deprecated
+    public static boolean addCustomEntry(JavaPlugin plugin, String action, Player player, Location loc, String data) {
+        return addCustomEntry(plugin, action, player.getName(), loc, data);
+    }
 
-	/**
-	 * Add a standard entry to the HawkEye database
-	 * @param plugin instance of your plugin (i.e. just pass 'this' from your main class)
-	 * @param type {@DataType} of the entry
-	 * @param player player that performed the action
-	 * @param loc location of the event
-	 * @param data data relevant to the event
-	 * @return true if accepted, false if not
-	 */
-	public static boolean addEntry(JavaPlugin plugin, DataEntry entry) {
+    @Deprecated
+    public static boolean addCustomEntry(JavaPlugin plugin, String action, String player, Location loc, String data) {
+        addCustomEntry(action, player, loc, data);
+        return true;
+    }
 
-		if (entry.getClass() != entry.getType().getEntryClass()) return false;
-		if (entry.getPlayer() == null) return false;
-		
-		DataManager.addEntry(entry);
-		return true;
+    public static void addCustomEntry(String action, String player, Location loc, String data) {
+        if (action == null || player == null || loc == null || data == null)
+            throw new NullPointerException();
 
-	}
+        addEntry(new DataEntry(player, DataType.OTHER, loc, action + "-" + data));
+    }
 
-	/**
-	 * Performs a search of the HawkEye database
-	 * @param callBack implementation of {@BaseCallback} for {@SearchQuery} to call
-	 * @param parser instance of {@SearchParser} to retrieve search information
-	 * @param dir direction to list results as specified in {@SearchDir}
-	 */
-	public static void performSearch(BaseCallback callBack, SearchParser parser, SearchDir dir) {
-		new SearchQuery(callBack, parser, dir);
-	}
+    /**
+     * Add a standard entry to the HawkEye database
+     *
+     * @return true if accepted, false if not
+     */
+    @Deprecated
+    public static boolean addEntry(JavaPlugin plugin, DataEntry entry) {
+        return addEntry(entry);
+    }
+
+    public static boolean addEntry(DataEntry entry) {
+
+        if (entry.getClass() != entry.getType().getEntryClass()) return false;
+        if (entry.getPlayer() == null) return false;
+
+        DataManager.addEntry(entry);
+        return true;
+
+    }
+
+    /**
+     * Performs a search of the HawkEye database
+     *
+     * @param callBack implementation of {@BaseCallback} for {@SearchQuery} to call
+     * @param parser   instance of {@SearchParser} to retrieve search information
+     * @param dir      direction to list results as specified in {@SearchDir}
+     */
+    public static void performSearch(BaseCallback callBack, SearchParser parser, SearchDir dir) {
+        new SearchQuery(callBack, parser, dir);
+    }
 
 }
